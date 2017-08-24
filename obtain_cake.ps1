527173910c653768833dcf18b74119ed3daba9da
Wolfgang Pankratz <wolfgangpankratz@gmail.com>
2017-08-24 19:41:52 +0200

    Bare-minimum GitVersion added Typo in obtain_cake.ps1 fixed

--- obtain_cake.ps1
+++ obtain_cake.ps1
@@ -1,7 +1,7 @@
 try {
     $cake = Invoke-WebRequest https://cakebuild.net/download/bootstrapper/windows -OutFile build.ps1 -PassThru
     if ($cake.statuscode -eq 200) {
-        Write-Host -ForegroundColor Green "Obtained latest Version of Cake"
+        Write-Host -ForegroundColor Green "Latest version of Cake obtained from GitHub"
     }
 } catch {
     Write-Warning -Message "Error obtaining latest Cake Bootstrapper"
