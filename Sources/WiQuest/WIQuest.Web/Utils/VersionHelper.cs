d90fb077bae36f545beb87e22ef353f7c152bebc
wpankratz <wolfgangpankratz@gmail.com>
2017-10-03 12:49:44 +0200

    VersionHelper updated

--- Sources/WiQuest/WIQuest.Web/Utils/VersionHelper.cs
+++ Sources/WiQuest/WIQuest.Web/Utils/VersionHelper.cs
@@ -13,13 +13,18 @@ namespace WIQuest.Web.Utils
         {
             try
             {
-                var fullSemVer = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
-                var semVer = SemVersion.Parse(fullSemVer);
+                return FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+
+
+                /*
+
+                var semVer =
+                    SemVersion.Parse("0.2.0-unstable.4+Branch.develop.Sha.9201bcddb426fd4dde7b73f48b6f4d2ab58917f7");
                 if (!semVer.Prerelease.IsEmpty())
                 {
                     return $"v{semVer.Major}.{semVer.Minor}.{semVer.Patch}.{semVer.Prerelease}";
                 }
-                return $"v{semVer.Major}.{semVer.Minor}.{semVer.Patch}";
+                return $"v{semVer.Major}.{semVer.Minor}.{semVer.Patch}";*/
             }
             catch (Exception)
             {
